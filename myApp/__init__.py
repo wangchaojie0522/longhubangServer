ffc33acbc23eb82d35b188682874f5f235275247
chaojiewang <chaojiewang@deepglint.com>
2023-07-11 10:14:52 +0800

    【功能完善】

---
+++ myApp/__init__.py
@@ -0,0 +1,11 @@
+'''
+Author: chaojiewang chaojiewang@deepglint.com
+Date: 2023-07-10 15:19:23
+LastEditors: chaojiewang chaojiewang@deepglint.com
+LastEditTime: 2023-07-10 18:47:48
+FilePath: /longhubangServer/myApp/__init__.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import pymysql
+
+pymysql.install_as_MySQLdb()
